1da153093d6c8599a5fd5d9561814443c50ba945
SongGwangSun <songgs33@gmail.com>
2025-12-25 16:03:51 +0700

    css 값정리

--- script.js
+++ script.js
@@ -276,7 +276,7 @@ function renderPlayerList() {
         item.style = `
             background: #fff;
             margin-bottom: 8px;
-            padding: 12px 24px;
+            padding: 12px;
             border-radius: 8px;
             box-shadow: 0 2px 4px rgba(0,0,0,0.05);
         `;
